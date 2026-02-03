ddc27e9f75705fab416ad8e433e7b28acf8031c5
Cindy Guo <cindyguo636@gmail.com>
2026-02-02 19:43:29 -0500

    all code delted, restart code from start

--- src/main/java/frc/robot/constants/variants/TutorialConstants.java
+++ src/main/java/frc/robot/constants/variants/TutorialConstants.java
@@ -1,23 +1,2 @@
 package frc.robot.constants.variants;
 
-import edu.wpi.first.math.util.Units;
-
-public class TutorialConstants {
-    public final static class PhotonConstants {
-            public static final String BW_FRONT_NAME = "ArducamBW2";
-            public static final double BW_FRONT_FORWARD = 0.396311;
-            public static final double BW_FRONT_LEFT = -0.266700 - Units.inchesToMeters(1 / 16.0);
-            public static final double BW_FRONT_UP = 0.254114;
-            public static final double BW_FRONT_ROLL = 0.0;
-            public static final double BW_FRONT_PITCH = -5.0;
-            public static final double BW_FRONT_YAW = -0.0;
-
-            public static final String BW_BACK_NAME = "ArducamBW3";
-            public static final double BW_BACK_FORWARD = -0.305367;
-            public static final double BW_BACK_LEFT = 0.266457;
-            public static final double BW_BACK_UP = 0.184669;
-            public static final double BW_BACK_ROLL = 0.0;
-            public static final double BW_BACK_PITCH = -8.0;
-            public static final double BW_BACK_YAW = 180;
-    }
-}

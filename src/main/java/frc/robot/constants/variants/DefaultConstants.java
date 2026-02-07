32f9ff4237b986cec588f81a676f19775ad838cd
Aj <ajiteshpika@gmail.com>
2026-02-07 16:53:51 -0500

    Added constants for 4 cameras :p

--- src/main/java/frc/robot/constants/variants/DefaultConstants.java
+++ src/main/java/frc/robot/constants/variants/DefaultConstants.java
@@ -2,4 +2,36 @@ package frc.robot.constants.variants;
 
 public class DefaultConstants {
 
+    public static String CAMERA_RED_NAME = "RedBW"; 
+    public static double CAMERA_RED_YAW = 0;
+    public static double CAMERA_RED_PITCH = 0;
+    public static double CAMERA_RED_ROLL = 0;
+    public static double CAMERA_RED_FORWARD = 0;
+    public static double CAMERA_RED_LEFT = 0;
+    public static double CAMERA_RED_UP = 0;
+
+    public static String CAMERA_GRAY_NAME = "GrayBW"; 
+    public static double CAMERA_GRAY_YAW = 180;
+    public static double CAMERA_GRAY_PITCH = 0;
+    public static double CAMERA_GRAY_ROLL = 0;
+    public static double CAMERA_GRAY_FORWARD = 0;
+    public static double CAMERA_GRAY_LEFT = 0;
+    public static double CAMERA_GRAY_UP = 0;
+
+    public static String CAMERA_GOLD_NAME = "GoldBW"; 
+    public static double CAMERA_GOLD_YAW = 0;
+    public static double CAMERA_GOLD_PITCH = 0;
+    public static double CAMERA_GOLD_ROLL = 0;
+    public static double CAMERA_GOLD_FORWARD = 0;
+    public static double CAMERA_GOLD_LEFT = 0;
+    public static double CAMERA_GOLD_UP = 0;
+
+    public static String CAMERA_BLACK_NAME = "BlackBW"; 
+    public static double CAMERA_BLACK_YAW = 180;
+    public static double CAMERA_BLACK_PITCH = 0;
+    public static double CAMERA_BLACK_ROLL = 0;
+    public static double CAMERA_BLACK_FORWARD = 0;
+    public static double CAMERA_BLACK_LEFT = 0;
+    public static double CAMERA_BLACK_UP = 0;
+
 }

a38caa95abbc18b2de8d87d90f3f604197781180
Cindy Guo <cindyguo636@gmail.com>
2026-02-14 10:49:10 -0500

    set up for all the subsyestems is done

--- src/main/java/frc/robot/subsystems/intake/Intake.java
+++ src/main/java/frc/robot/subsystems/intake/Intake.java
@@ -1,32 +1,41 @@
 package frc.robot.subsystems.intake;
 
+import java.io.ObjectInputFilter.Config;
+
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.VelocityVoltage;
 import com.ctre.phoenix6.controls.VoltageOut;
 import com.ctre.phoenix6.hardware.TalonFX;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 import frc.robot.subsystems.spindexer.Spindexer;
 
-public class Intake {
-    private final TalonFX Kraken = new TalonFX(0);
-
+public class Intake extends SubsystemBase{
+    private final TalonFX DeployKraken = new TalonFX(52);
+    private final TalonFX IntakeKraken = new TalonFX(56);
     public Intake() {
-        Kraken.getConfigurator().apply(new TalonFXConfiguration());
+        DeployKraken.getConfigurator().apply(new TalonFXConfiguration());
+        IntakeKraken.getConfigurator().apply(new TalonFXConfiguration());
         TalonFXConfiguration config = new TalonFXConfiguration();
-        Kraken.getConfigurator().apply(config);        
+        DeployKraken.getConfigurator().apply(config); 
+        IntakeKraken.getConfigurator().apply(config);     
     }
     private final VoltageOut voltageControl = new VoltageOut(0);
 
     public void setVoltage(double volts) {
-        Kraken.setControl(voltageControl.withOutput(volts));
-        final VelocityVoltage velocityControl = new VelocityVoltage(0);
+        DeployKraken.setControl(voltageControl.withOutput(volts));
+        IntakeKraken.setControl(voltageControl.withOutput(volts));
+        
     }
 
     private final VelocityVoltage velocityControl = new VelocityVoltage(0);
 
 
     public void setVelocity(double rotationsPerSecond) {
-        Kraken.setControl(
+        DeployKraken.setControl(
+            velocityControl.withVelocity(rotationsPerSecond)
+        );
+        IntakeKraken.setControl(
             velocityControl.withVelocity(rotationsPerSecond)
         );
     }

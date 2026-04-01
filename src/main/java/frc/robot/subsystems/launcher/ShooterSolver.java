485f2aa11aaeadff41230ebe91546fa698ddf6e4
saina-hi <sainakarve@gmail.com>
2026-04-01 19:06:33 -0400

    changing path rotation- they should workj
    :p

--- src/main/java/frc/robot/subsystems/launcher/ShooterSolver.java
+++ src/main/java/frc/robot/subsystems/launcher/ShooterSolver.java
@@ -34,8 +34,8 @@ public class ShooterSolver {
     public static final double FIELD_LENGTH_METERS = 16.5417; 
     
     // Blue Alliance Hub Coordinates
-    public static final double BLUE_TARGET_X = 4.45; // 4.62 [m]
-    public static final double BLUE_TARGET_Y = 4.17; // 4.02 [m]
+    public static final double BLUE_TARGET_X = 4.65; // 4.62 [m]
+    public static final double BLUE_TARGET_Y = 4.35; // 4.02 [m]`   
 
     // Ball Physics
 
